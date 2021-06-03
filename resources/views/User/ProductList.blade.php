f07f36b87c8db93d304ee75225ff5e5cb36a34bc
vishnukmwiz <vishnukmwiz@gmail.com>
2021-06-03 22:39:22 +0530

    first commit

--- resources/views/User/ProductList.blade.php
+++ resources/views/User/ProductList.blade.php
@@ -106,7 +106,7 @@
                   <ul class="list-unstyled ps-2">
                     <li class="mb-1">
                       <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse-sub" aria-expanded="false">
-                        Home
+                        
                       </button>
                       <div class="collapse" id="home-collapse-sub">
                         <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
